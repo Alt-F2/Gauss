561eb7de18ca371583220d60bee625ce48ef27f1
A-Ized <55930001+Alt-F2@users.noreply.github.com>
2022-05-10 14:09:52 +0100

    Update MPI.cpp

--- MPI.cpp
+++ MPI.cpp
@@ -1,7 +1,8 @@
 // Name: Andrew Izedomwen, Student ID: 201187740
 
 #include <stdio.h>
-#include <omp.h>
+#include <mpi.h>
+#include <time.h>
 
 int main() {
 
@@ -16,17 +17,23 @@ int main() {
 	double x[3];
 	int k = 0, i = 0, j = 0;
 
+	clock_t timer = clock();
+
+	int ranking, processorNum, part;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ranking);
+	MPI_Comm_size(MPI_COMM_WORLD, &processorNum);
+	part = n / processorNum;
+
 	printf("\n ================= Forward Elimination Step =================");
 
 	// Forward Elimination
-	for (k = 0; k < n - 1; k++)
+	for (k = ranking * part; k < ranking * part + part; k++)
 	{
-		#pragma omp parallel shared(mA, mB, n, k) private(i, j) for  
-		for (i = k + 1; i < n; i++)
+		for (i = k + 1; i < ranking * part + part; i++)
 		{
-			#pragma omp parallel shared(mA, mB, n, k, i) private(j) for
 			double factor = mA[i][k] / mA[k][k];
-			for (j = k; j < n; j++)
+			for (j = k; j < ranking * part + part; j++)
 			{
 				mA[i][j] -= factor * mA[k][j];
 			}
@@ -70,7 +77,7 @@ int main() {
 	// result of X
 	for (i = 0; i < n; i++)
 	{
-		printf("\nResults of X%d: %.2f ", i+1, x[i]);
+		printf("\nResults of X%d: %.2f ", i + 1, x[i]);
 	}
 
 	printf("\n");
